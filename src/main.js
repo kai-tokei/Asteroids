794100a4e53bcecaab060e4e4b02a11f80325a76
村上律 <murakamitadashi@murakamitadashis-MacBook-Air.local>
2023-09-20 10:54:17 +0900

    add: gamescn

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ function setup() {
   canvas.parent("canvas");
 
   // system
-  scene = "title";
+  //scene = "title";
+  scene = "game";   // 開発用でgameにしておく
   titleScn = new TitleScn();
-  //gameScn = new GameScn();
+  gameScn = new GameScn();
 }
 
 function draw() {
@@ -22,7 +23,24 @@ function draw() {
   }
 }
 
+function mousePressed() {
+  switch (scene) {
+    case "title":
+      titleScn.mouse_pressed();
+      break;
+  }
+}
+
+function keyPressed() {
+  switch (scene) {
+    case "game":
+      gameScn.key_pressed();
+      break;
+  }
+}
+
 // ---- Scene ----
+
 class TitleScn {
   constructor() {
     this.titleChr = loadImage("../img/title.png");
@@ -47,15 +65,56 @@ class TitleScn {
       image(this.clickToStart, -20, 0);
     }
   }
+ 
+  mouse_pressed() {
+    scene = "game";
+  }
 }
 
-/*
+
 class GameScn() {
+  constructor() {
+
+  }
+
+  display() {
+    background(33, 39, 46);
+  }
+
 
 }
-*/
 
 // ---- obj classe ----
+
+class Player {
+  constructor(_x, _y, _theta) {
+    this.x = _x;
+    this.y = _y;
+    this.theta = _theta;
+
+    this.r = 20;
+  }
+
+  display() {
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+
+    // playerが向いている方方向
+    let x1 = this.x + this.r * cos(this.theta);
+    let y1 = this.y + this.r * sin(this.theta);
+
+    // 左舷
+    let x2 = this.x + this.r / 2 * cos(this.theta + (120 / 180) * PI); 
+    let y2 = this.y + this.r / 2 * sin(this.theta + (120 / 180) * PI); 
+
+    // 右舷
+    let x3 = this.x + this.r / 2 * cos(this.theta - (120 / 180) * PI); 
+    let y3 = this.y + this.r / 2 * sin(this.theta - (120 / 180) * PI); 
+
+    triangle(x1, y1, x2, y2, x3, y3);
+  }
+}
 
 class Timer {
   constructor(_lim) {
