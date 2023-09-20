8724c81abaff6ab8b718e93b42cc3ab27c296809
村上律 <murakamitadashi@murakamitadashis-MacBook-Air.local>
2023-09-20 11:49:40 +0900

    add: player rotation

--- src/main.js
+++ src/main.js
@@ -2,25 +2,27 @@
 let scene;
 let titleScn;
 let gameScn;
-let keys;
 
 // ---- system ----
 function setup() {
   let canvas = createCanvas(640, 480);
   canvas.parent("canvas");
+  frameRate(60);
 
   // system
   //scene = "title";
   scene = "game";   // 開発用でgameにしておく
   titleScn = new TitleScn();
   gameScn = new GameScn();
-  keys = new Key();
 }
 
 function draw() {
   switch (scene) {
     case "title":
       titleScn.display();
+      break;
+    case "game":
+      gameScn.display();
       break;
   }
 }
@@ -34,9 +36,17 @@ function mousePressed() {
 }
 
 function keyPressed() {
-  switch (scene) {
+  switch(scene) {
     case "game":
       gameScn.key_pressed();
+      break;
+  }
+}
+
+function keyReleased() {
+  switch(scene) {
+    case "game":
+      gameScn.key_released();
       break;
   }
 }
@@ -74,27 +84,42 @@ class TitleScn {
 }
 
 
-class GameScn() {
+class GameScn {
   constructor() {
-
+    this.player = new Player(100, 100, 0);
   }
 
   display() {
     background(33, 39, 46);
+
+    this.player.display();
+    this.player.rudder(5);
   }
 
+  key_pressed() {
+    this.player.key_pressed();
+  }
 
+  key_released() {
+    this.player.key_released();
+  }
 }
 
 // ---- obj classe ----
 
 class Player {
-  constructor(_x, _y, _theta) {
+  constructor(_x, _y, _deg_theta) {
+    // pos
     this.x = _x;
     this.y = _y;
-    this.theta = _theta;
+    
+    // angle
+    this.deg_theta = _deg_theta;
+    this.rad_theta = this.deg_to_rad(this.deg_theta);
 
-    this.r = 20;
+    this.r = 30;
+
+    this.keys = new Keys();
   }
 
   display() {
@@ -103,34 +128,50 @@ class Player {
     strokeWeight(2);
 
     // playerが向いている方向
-    let x1 = this.x + this.r * cos(this.theta);
-    let y1 = this.y + this.r * sin(this.theta);
+    let x1 = this.x + this.r * cos(this.rad_theta);
+    let y1 = this.y + this.r * sin(this.rad_theta);
 
     // 左舷
-    let x2 = this.x + this.r / 2 * cos(this.theta + (120 / 180) * PI); 
-    let y2 = this.y + this.r / 2 * sin(this.theta + (120 / 180) * PI); 
+    let x2 = this.x + this.r / 2 * cos(this.rad_theta + (120 / 180) * PI); 
+    let y2 = this.y + this.r / 2 * sin(this.rad_theta + (120 / 180) * PI); 
 
     // 右舷
-    let x3 = this.x + this.r / 2 * cos(this.theta - (120 / 180) * PI); 
-    let y3 = this.y + this.r / 2 * sin(this.theta - (120 / 180) * PI); 
+    let x3 = this.x + this.r / 2 * cos(this.rad_theta - (120 / 180) * PI); 
+    let y3 = this.y + this.r / 2 * sin(this.rad_theta - (120 / 180) * PI); 
 
     triangle(x1, y1, x2, y2, x3, y3);
   }
 
-  key_pressed() {
-    this.rudder(1);
+  rudder(_power) {
+    stroke(255);
+    text(this.deg_theta, 100, 400);
+
+    if (this.keys.isL) this.deg_theta -= _power;
+    if (this.keys.isR) this.deg_theta += _power;
+    
+    if (this.deg_theta > 180) this.deg_theta -= 360;
+    else if (this.deg_theta < -180) this.deg_theta += 360;
+
+    this.rad_theta = this.deg_to_rad(this.deg_theta);
   }
 
-  rudder(_power) {
-    switch(keyCode) {
-      case 65:  // aで取り舵
-        this.theta += (_power / 180) * PI;
-        break;
-      case 67:  // dで面舵
-        this.theta -= (_power / 180) * PI;
-    }
+  deg_to_rad(_deg) {
+    return _deg / 180 * PI;
+  }
+
+  rad_to_deg(_rad) {
+    return _rad / PI * 180;
+  }
+
+  key_pressed() {
+    this.keys.key_pressed();
+  }
+
+  key_released() {
+    this.keys.key_released();
   }
 }
+
 
 class Timer {
   constructor(_lim) {
@@ -153,6 +194,7 @@ class Timer {
     }
   }
 }
+
  
 class Keys {
   constructor() {
@@ -165,11 +207,13 @@ class Keys {
     if (key == 'w') this.isU = true;
     if (key == 'a') this.isL = true;
     if (key == 's') this.isD = true;
+    if (key == 'd') this.isR = true;
   }
 
   key_released() {
     if (key == 'w') this.isU = false;
     if (key == 'a') this.isL = false;
     if (key == 's') this.isD = false;
+    if (key == 'd') this.isR = false;
   }
 }
