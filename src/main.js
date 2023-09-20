864de15b8d3e48bdacaaa1eded63dcd7c08ea576
村上律 <murakamitadashi@murakamitadashis-MacBook-Air.local>
2023-09-20 12:06:06 +0900

    add: particle

--- src/main.js
+++ src/main.js
@@ -224,9 +224,23 @@ class Timer {
   }
 }
 
-class perticle {
-  constructor(_x, _y, _r) {
+class Particle {
+  constructor(_x, _y, _theta) {
     this.x = _x;
+    this.y = _y;
+    this.theta = _theta;
+    this.r = 5;
+    
+    this.timer = new Timer(10);
+    this.exist = true;
+  }
+
+  display() {
+    if (this.timer.cnt()) this.exist = false;
+
+    noStroke();
+    fill(255 - this.timer.time * 10);
+    ellipse(this.x, this.r, this.r, this.r);
   }
 }
  
